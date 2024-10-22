cdb33f811c304bee36c6fe4da5a903a0369a9fcf
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-22 19:04:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
-
+	const strongs = document.querySelectorAll("strong");
+	//console.log(strongs);
+	strongs.forEach((strong) => {
+		strong.style.color= "green"
+	})
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+    const strongs = document.querySelectorAll("strong");
+	//console.log(strongs);
+	strongs.forEach((strong) => {
+		strong.style.color= "black"
+	})
 }
